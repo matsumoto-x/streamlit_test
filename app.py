72d74f0b1afe48183f9f2505d2a26cf502672ab6
matsumoto-x <109731634+matsumoto-x@users.noreply.github.com>
2023-06-23 22:54:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
+# import streamlit as st
+# from multiapp import MultiApp
+# from apps import home, data_stats
+
+# app = MultiApp() 
+
+# app.add_app("Home", home.app)
+# app.add_app("Data Stats", data_stats.app) 
+
+# app.run()
+
 import streamlit as st
-from multiapp import MultiApp
-from apps import home, data_stats
 
-app = MultiApp() 
-
-app.add_app("Home", home.app)
-app.add_app("Data Stats", data_stats.app) 
-
-app.run()
+st.title("title") # タイトル
+st.header("header") # ヘッダー
+st.write("write") # 表示
+st.markdown("# markdown") # マークダウンで表示
+st.text("text") # テキスト表示
